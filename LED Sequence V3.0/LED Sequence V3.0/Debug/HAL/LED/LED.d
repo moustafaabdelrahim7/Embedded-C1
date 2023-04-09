82d7b37b20f878fb98f909e551eb5e26130fab77
moustafaabdelrahim7 <128411699+moustafaabdelrahim7@users.noreply.github.com>
2023-04-09 04:46:42 +0200

    Add files via upload

---
+++ LED Sequence V3.0/LED Sequence V3.0/Debug/HAL/LED/LED.d
@@ -0,0 +1,15 @@
+HAL/LED/LED.d HAL/LED/LED.o: ../HAL/LED/LED.c ../HAL/LED/LED.h \
+ ../HAL/LED/../../MCAL/DIO/DIO.h \
+ ../HAL/LED/../../MCAL/DIO/../../SERVICE/UTILES.h \
+ ../HAL/LED/../../MCAL/DIO/../MEMORYMAPIING.h \
+ ../HAL/LED/../../MCAL/DIO/../../SERVICE/STD_TYPES.h
+
+../HAL/LED/LED.h:
+
+../HAL/LED/../../MCAL/DIO/DIO.h:
+
+../HAL/LED/../../MCAL/DIO/../../SERVICE/UTILES.h:
+
+../HAL/LED/../../MCAL/DIO/../MEMORYMAPIING.h:
+
+../HAL/LED/../../MCAL/DIO/../../SERVICE/STD_TYPES.h:
---
+++ LED Sequence V3.0/LED Sequence V3.0/HAL/LED/LED.c
@@ -0,0 +1,61 @@
+﻿#include "LED.h"
+LED_ERROR_TYPE LED_INIT(DIO_PIN_TYPE PIN)
+{
+	DIO_PIN_TYPE PIN_NUM;
+	DIO_PORT_TYPE PORT;
+	PORT=PIN/8;
+	PIN_NUM=PIN%8;
+	if(PORT>4)
+	{
+		return INVALID_LED_PORT;
+	}
+	else if(PIN_NUM>32)
+	{
+		return INVLAID_LED_PIN_NUMBER;
+	}
+	else
+	{
+		DIO_INITPIN(PIN,OUTPUT);
+		return VALID_LED;
+	}
+}
+LED_ERROR_TYPE LED_ON(DIO_PIN_TYPE PIN)
+{
+	DIO_PIN_TYPE PIN_NUM;
+	DIO_PORT_TYPE PORT;
+	PORT=PIN/8;
+	PIN_NUM=PIN%8;
+	if(PORT>4)
+	{
+		return INVALID_LED_PORT;
+	}
+	else if(PIN_NUM>32)
+	{
+		return INVLAID_LED_PIN_NUMBER;
+	}
+	else
+	{
+		DIO_WRITEPIN(PIN,HIGH);
+		return VALID_LED;
+	}
+}
+LED_ERROR_TYPE LED_OFF(DIO_PIN_TYPE PIN)
+{
+	DIO_PIN_TYPE PIN_NUM;
+	DIO_PORT_TYPE PORT;
+	PORT=PIN/8;
+	PIN_NUM=PIN%8;
+	if(PORT>4)
+	{
+		return INVALID_LED_PORT;
+	}
+	else if(PIN_NUM>32)
+	{
+		return INVLAID_LED_PIN_NUMBER;
+	}
+	else
+	{
+		DIO_WRITEPIN(PIN,LOW);
+		return VALID_LED;
+	}
+}
